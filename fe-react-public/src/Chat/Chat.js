a6c7c22c1e533721f04d847466f72cba8ae72cd2
glomotion <tim@glo.id.au>
2020-10-23 16:03:40 +1100

    refactoring to satisfy eslint

--- fe-react-public/src/Chat/Chat.js
+++ fe-react-public/src/Chat/Chat.js
@@ -1,27 +1,23 @@
 import React, { useState, useEffect } from "react";
-import update from "immutability-helper";
 import "react-chat-elements/dist/main.css";
 
 const Chat = (props) => {
   const [chatState, setChatState] = useState(["STARTING TEXT"]);
   const addMessage = (msg) => {
-    setChatState((prevChatState) => update(prevChatState, { $push: [msg] }));
+    setChatState((prevChatState) => [...prevChatState, msg]);
   };
 
-  const handleSocketMsg = (evt) => {
-    var msgs = evt.data.split("\n");
-    for (var i = 0; i < msgs.length; i++) {
-      addMessage(msgs[i]);
-      console.log(chatState);
-    }
-  };
   useEffect(() => {
     console.log("this is only executed when it's created");
-    const conn = new WebSocket("ws://localhost:8000/ws");
-
-    conn.onmessage = handleSocketMsg;
-    conn.onopen = (event) => {
+    const socket = new WebSocket("ws://localhost:8000/ws");
+    socket.onopen = (event) => {
       console.log("WebSocket is open now.");
+    };
+    socket.onmessage = (evt) => {
+      var msgs = evt.data.split("\n");
+      for (var i = 0; i < msgs.length; i++) {
+        addMessage(msgs[i]);
+      }
     };
   }, []);
 
